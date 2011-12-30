ab5621e0934166fcb803f2b871241750ce195958
Stephen Compall <scompall@nocandysw.com>
2011-12-30 17:52:44 -0500

    Require Leiningen 1.6.0+.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject kiln "1.0.0-SNAPSHOT"
   :description "A computational strategy for large, ugly applications"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [slingshot "0.10.0"]])
+                 [slingshot "0.10.0"]]
+  :min-lein-version "1.6.0")
