7af375249f14969a18b21fa04cbaa7fdf60f403a
Jeffrey Straszheim <straszheimjeffrey@gmail.com>
2012-05-04 20:51:19 -0400

    Bumped version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kiln "0.0.1"
+(defproject kiln "0.0.2"
   :description "A computational strategy for large, ugly applications"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [slingshot "0.10.2"]]
