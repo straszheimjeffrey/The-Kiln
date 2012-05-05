9c4eb2e49f54cfa8de81d3c1b589f11e13184fff
Jeffrey Straszheim <straszheimjeffrey@gmail.com>
2012-05-04 22:45:03 -0400

    URI Work

--- sample/src/sample/uri_utils.clj
+++ sample/src/sample/uri_utils.clj
@@ -1,25 +1,28 @@
 (ns
-    ^{:doc "Some URI Utilities"
+    ^{:doc "Some URI Utilities
+
+This code provides a simple implementation of the Clojure Associative
+interface which wraps the Java URI object. It allows you to process it
+such as:
+
+ (:host uri)
+
+and
+
+ (assoc :path uri)
+
+It supports .toString as normal. It also supports proper equality
+semantics.
+
+The methods as-uri and as-java-uri are provided for convinient
+conversion.
+"
       :author "Jeffrey Straszheim"}
-  uri-utils
+  sample.uri-utils
   (import java.net.URI))
 
 (defprotocol uri-underlying-value
-  (java-uri [self]))
-
-(defn java-uri-absolute?
-  [java-uri]
-  (.getScheme java-uri))
-
-(defn java-uri-opaque?
-  [java-uri]
-  {:pre [(java-uri-absolute? java-uri)]}
-  (if-let [ssp (.getRawSchemeSpecificPart java-uri)]
-    (not= (first ssp) "/")))
-
-(defn java-uri-hierarchical [java-uri]
-  (or (not (java-uri-absolute? java-uri))
-      (not (java-uri-opaque? java-uri))))
+  (java-uri [self] "Get the underlying java.net.URI object"))
     
 (deftype uri [java-uri]
 
@@ -59,7 +62,7 @@
       :user-info (.getRawUserInfo java-uri)
       :authority (.getRawAuthority java-uri)
       :scheme-specific-part (.getRawSchemeSpecificPart java-uri)
-      :ssp  (.getRawSchemeSpecificPart java-uri)
+      :ssp (.getRawSchemeSpecificPart java-uri)
       nil))
   (valAt [self key not-found]
     (if-let [result (.valAt self key)] result not-found))
@@ -73,11 +76,20 @@
     (.toString java-uri)))
 
 (defn as-uri
- [obj]
- (cond
-  (instance? uri obj) obj
-  (instance? URI obj) (uri. obj)
-  (string? obj) (uri. (URI. obj))))
+  "Return obj converted to a uri-utils.uri object."
+  [obj]
+  (cond
+   (instance? uri obj) obj
+   (instance? URI obj) (uri. obj)
+   (string? obj) (uri. (URI. obj))))
+
+(defn as-java-uri
+  "Return obj converted to a java.net.URI object."
+  [obj]
+  (cond
+   (instance? uri obj) (java-uri obj)
+   (instance? URI obj) obj
+   (string? obj) (URI. obj)))
     
 
 ;; End of file
