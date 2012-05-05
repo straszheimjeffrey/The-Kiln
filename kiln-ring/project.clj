2c7f08bd448e017d4e0c6121069c6ac1e56e8d5c
Jeffrey Straszheim <straszheimjeffrey@gmail.com>
2012-05-05 19:08:46 -0400

    1.0

--- kiln-ring/project.clj
+++ kiln-ring/project.clj
@@ -1,6 +1,6 @@
-(defproject kiln/kiln-ring "0.0.1"
+(defproject kiln/kiln-ring "1.0.0"
   :description "A Small Adapter to link Kiln with the Ring."
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [kiln "0.0.2"]
+                 [kiln "1.0.0"]
                  [ring "1.1.0"]]
   :dev-dependencies [[swank-clojure "1.3.3"]])
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kiln "0.0.2"
+(defproject kiln "1.0.0"
   :description "A computational strategy for large, ugly applications"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [slingshot "0.10.2"]]
