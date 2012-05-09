04a68b298efb88bfa14252f85c6c0fb08af2cbf8
Jeffrey Straszheim <straszheimjeffrey@gmail.com>
2012-05-08 21:42:45 -0400

    Performance

--- src/kiln/perf.clj
+++ src/kiln/perf.clj
@@ -17,11 +17,23 @@
 
 (defn run-long-test
   [which]
+  (println (format "Long test %s" which))
   (time (run-it 100000 which)))
 
 (defn run-short-test
   [which]
+  (println (format "Short test %s" which))
   (time (run-it 1000 which)))
 
+(comment
+
+(do
+  (println)
+  (run-long-test :ref)
+  (run-long-test :atom)
+  (run-short-test :ref)
+  (run-short-test :atom))
+
+)
 
 ;; End of file
